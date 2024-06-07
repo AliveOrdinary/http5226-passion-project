76e38a9959fa7a8dcb6930e8beb08fe0ff183f10
Navaneeth Krishnan <vrnavaneeth135@gmail.com>
2024-06-07 17:23:50 -0400

    --project started

---
+++ passion-projectMVP/passion-projectMVP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="passion_projectMVP.MvcApplication" Language="C#" %>
---
+++ passion-projectMVP/passion-projectMVP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(passion_projectMVP.Startup))]
+namespace passion_projectMVP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
